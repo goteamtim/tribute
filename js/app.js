a2160c3dd14a49e7a6fa50d5f617f5581a1558c7
Tim Harshman <goteamtim+git@gmail.com>
2017-01-15 12:21:35 -0800

    Added more JS need to learn more about ngRoute

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
-var app = angular.module('tributeApp', []);
+var app = angular.module('tributeApp', ["ngRoute"]);
 
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/:user', {
+        templateUrl: './index.html',
+        controller:'GitHubController'
+    })
+    .when('/',{
+        templateUrl: './index.html',
+        controller: 'GitHubController'
+    })
 
-app.controller('GitHubController', ['$scope', '$http', function ($scope, $http) {
+        .otherwise({
+        redirectTo: '/',
+    });
     
+});
+
+
+app.controller('GitHubController', ['$scope', '$http','$routeParams', function ($scope, $http, $routeParams) {
+    if($routeParams.user != null){
+        alert("UserFound!");
+    }
+
+    $scope.getUserInfo = function getUserInfo(user) {
+        $http({
+            method: 'GET',
+            url: 'http://api.github.com/users/' + user
+        }).then(function successCallback(response) {
+            
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
 }]);
