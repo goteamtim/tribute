9583310c1c6b612dd501736a725f84e245ad0815
Tim Harshman <goteamtim+git@gmail.com>
2017-02-02 22:00:54 -0800

    Starting start implimentation

--- js/app.js
+++ js/app.js
@@ -130,6 +130,31 @@ app.controller('repoController', ['$scope', '$http', 'gitHubInfo', '$rootScope',
 
     }
 
+    function plotGraph() {
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Task', 'Hours per Day'],
+                ['Work', 11],
+                ['Eat', 2],
+                ['Commute', 2],
+                ['Watch TV', 2],
+                ['Sleep', 7]
+            ]);
+
+            var options = {
+                title: 'My Daily Activities'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+        }
+    }
+
+    plotGraph();
 
     Object.defineProperty(Object.prototype, 'addObject', {
         value: function (newObject) {
